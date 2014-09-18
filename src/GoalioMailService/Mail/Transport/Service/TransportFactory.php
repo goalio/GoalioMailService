16732ecdd2e43acc418e0541e2606c6abc2140db
Stefan Kleff <s.kleff@goalio.de>
2014-09-18 10:30:20 +0200

    Update TransportFactory.php
    Use new ZF2 Transport Factory

--- src/GoalioMailService/Mail/Transport/Service/TransportFactory.php
+++ src/GoalioMailService/Mail/Transport/Service/TransportFactory.php
@@ -1,8 +1,6 @@
 <?php
 namespace GoalioMailService\Mail\Transport\Service;
 
-use GoalioMailService\Mail\Options\TransportOptions;
-use Zend\Mail\Transport\TransportInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
 use Zend\ServiceManager\FactoryInterface;
 
@@ -11,24 +9,8 @@ class TransportFactory implements FactoryInterface {
     public function createService(ServiceLocatorInterface $serviceLocator) {
 
         /** @var TransportOptions $options */
-        $options = $serviceLocator->get('goaliomailservice_options');
-
-        if(!class_exists($options->getTransportClass())) {
-            throw new \Exception('Transport class has to be configured');
-        }
-
-        $transportClass = $options->getTransportClass();
-
-        /** @var TransportInterface $transport */
-        $transport = new $transportClass;
-
-        if(class_exists($options->getOptionsClass())) {
-            $transportOptionsClass = $options->getOptionsClass();
-            $transportOptions = new $transportOptionsClass;
-            $transportOptions->setFromArray($options->getTransportOptions());
-            $transport->setOptions($transportOptions);
-        }
-
-        return $transport;
+        $config = $serviceLocator->get('config');
+        $options = $config['goaliomailservice'];
+        return \Zend\Mail\Transport\Factory::create($options);
     }
 }
